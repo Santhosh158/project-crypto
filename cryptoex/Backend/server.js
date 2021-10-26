b17f3fb81b891d55a108098d863f8d614904e389
Santhosh Babu <santhosh15895@gmail.com>
2021-10-26 00:56:11 -0400

    heroku port

--- cryptoex/Backend/server.js
+++ cryptoex/Backend/server.js
@@ -6,7 +6,7 @@ import path from 'path';
 const app = express();
 const __dirname = path.resolve();
 
-app.use('/', express.static(path.join(__dirname, '/Frontend/public')));
+
 
 app.get('/api/Ex1', (req,res) => {
 
@@ -25,13 +25,21 @@ app.get('/api/Ex2', (req,res) => {
     })
 });
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('Frontend/public'));
+    
 app.get('/',(req,res) =>{
     res.send('Server is ready');
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname,"Frontend","public","index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname,"Frontend","public","index.html"));
+// });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'Frontend', 'public', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
